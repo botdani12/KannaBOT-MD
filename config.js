1d092d0c2fb6fe23bb0c3d108569caf73e525881
botdani12 <104151646+botdani12@users.noreply.github.com>
2022-06-19 21:46:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@
 // • The.Sad.Boy01
 // • Rasel comel
 // • Xtreshe (Beban)
+// • DaniBotz
 // • Dll
 
 //[!] Jangan Dihapus, mending ditambahin 
@@ -46,31 +47,31 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 global.sig = 'https://instagram.com/itsme_nitsu'
 global.sgh = 'http://github.com/JakaV4'
-global.sgc = 'https://chat.whatsapp.com/ED7th5Ie38eBayZEbDPxwY'
+global.sgc = 'https://chat.whatsapp.com/BiJs5Xl4oJA1Ama5BahUwd'
 global.sdc = 'https://instagram.com/itsme_nitsu'
 global.snh = 'https://nhentai.net/g/365296/'
 
 /*============== PAYMENT ==============*/
-global.pdana = '081335334848'
-global.povo = '-'
-global.pgopay = '085785694474'
-global.ppulsa = '081335334848'
-global.ppulsa2 = '081335334848'
+global.pdana = '081251549899'
+global.povo = '081352848936'
+global.pgopay = '081352848936'
+global.ppulsa = '081352848936'
+global.ppulsa2 = '081251549899'
 global.psaweria = '-'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '79111309292'
-global.nomorown = '6285785694474'
-global.namebot = 'VīoLaꪶ͢Botzꫂ⁩'
-global.nameown = 'ζλκλꪶ͢Kunꫂ⁩'
+global.nomorbot = '6282122977451'
+global.nomorown = '6281251549899'
+global.namebot = 'DaniBotzꫂ⁩'
+global.nameown = 'DaniYgyꫂ⁩'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6285785694474'],
-  ['6285785694474'],
-  ['6285785694474'],
-  ['6285785694474', 'LETTA ! 🐰', true]
+  ['6281251549899'],
+  ['6281251549899'],
+  ['6281251549899'],
+  ['6281251549899', 'LETTA ! 🐰', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -116,13 +117,13 @@ global.APIKeys = { // APIKey Here
 
 /*============== WATERMARK ==============*/
 //GANTI SETERAH MU
-global.wm = '                「 VīoLaꪶ͢Botzꫂ⁩ あ⁩ 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘VīoLaꪶ͢Chanꫂ⁩˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ VīoLaꪶ͢Botzꫂ⁩'
+global.wm = '                「 Daniꪶ͢Botzꫂ⁩ あ⁩ 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘Daniꪶ͢Botzꫂ⁩˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ Daniꪶ͢Botzꫂ⁩'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
 global.titlebot = '🌱 ┊ 𝗥𝗣𝗚 Whatsapp ʙᴏᴛ'
-global.author = '                「 VīoLaꪶ͢Botzꫂ⁩ あ⁩ 」'
+global.author = '                「 Daniꪶ͢Botzꫂ⁩ あ⁩ 」'
 
 
 /*============== LOGO ==============*/
